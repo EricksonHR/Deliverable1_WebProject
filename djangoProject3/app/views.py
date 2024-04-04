4a840bb9082cca7695853401a4fd2dfbc6e44eb7
Neus <neus.fdez.valls@gmail.com>
2024-04-04 14:42:16 +0200

    Frontend :)

--- djangoProject3/app/views.py
+++ djangoProject3/app/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, JsonResponse
 
 
@@ -10,6 +12,31 @@ def hello_world(request):
     # return HttpResponse("Hello world!")
     return render(request, 'hello_world.html')
 
+def login(request):
+    # return HttpResponse("Hello world!")
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirigir a alguna página después del registro exitoso
+            return redirect('login')  # Redirigir a la página de inicio de sesión
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def welcome(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirigir a alguna página después del registro exitoso
+            return redirect('login')  # Redirigir a la página de inicio de sesión
+    else:
+        form = UserCreationForm()
+    return render(request, 'welcome.html', {'form': form})
 
 '''
 def get_students(request):
--- djangoProject3/djangoProject3/urls.py
+++ djangoProject3/djangoProject3/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import hello_world
+from app.views import hello_world, login, register, welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello_world)
+    #path('', hello_world)
+    path('', login),
+    path('register/', register),
+    path('welcome/', welcome)
 ]
