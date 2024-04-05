13bec9d18cc7c1597638b874a0b21b5475ca2768
maria <mariazcona2017@gmail.com>
2024-04-05 12:45:59 +0200

    Front-end v2

--- djangoProject3/app/views.py
+++ djangoProject3/app/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponse, JsonResponse
-
-
-# from .models import Student
-
-# Create your views here.
 
 def hello_world(request):
-    # return HttpResponse("Hello world!")
     return render(request, 'hello_world.html')
 
 def login(request):
-    # return HttpResponse("Hello world!")
     return render(request, 'login.html')
 
 def register(request):
@@ -21,8 +13,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            # Redirigir a alguna página después del registro exitoso
-            return redirect('login')  # Redirigir a la página de inicio de sesión
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
@@ -32,15 +23,7 @@ def welcome(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            # Redirigir a alguna página después del registro exitoso
-            return redirect('login')  # Redirigir a la página de inicio de sesión
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'welcome.html', {'form': form})
-
-'''
-def get_students(request):
-    students = Student.objects.all()
-    student_values = list(students.values())
-    return JsonResponse(student_values, safe=False)
-'''
